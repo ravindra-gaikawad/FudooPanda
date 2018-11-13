5c22314e23c68ddb4254d6eae6767c837983643e
ravindra.gaikawad <ravindra.gaikwad@mobiliya.com>
2018-11-13 15:23:25 +0530

    Add project files.

---
+++ FudooPanda/FudooPanda/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FudooPanda.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
